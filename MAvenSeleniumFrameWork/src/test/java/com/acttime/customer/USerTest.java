45a09a7619ac6d339fc3e5330a57afca8b814400
GOA <GOA@GOA-PC>
2018-12-27 17:20:54 +0530

    2 new test

---
+++ MAvenSeleniumFrameWork/src/test/java/com/acttime/customer/USerTest.java
@@ -0,0 +1,15 @@
+package com.acttime.customer;
+
+import org.testng.annotations.Test;
+
+public class USerTest {
+	@Test
+	public void createUSerTest(){
+		System.out.println("execute createUSerTest");
+	}
+
+	@Test
+	public void modifyuserTest(){
+		System.out.println("execute modifyuserTest");
+	}
+}
