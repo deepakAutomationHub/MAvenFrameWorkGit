ffcb8a8ed479ad58df0114dc14808b1fc7d91a33
GOA <GOA@GOA-PC>
2018-12-27 17:04:16 +0530

    fdsfdf

---
+++ MAvenSeleniumFrameWork/src/test/java/com/acttime/customer/CustomerTest.java
@@ -0,0 +1,26 @@
+package com.acttime.customer;
+
+import org.testng.annotations.Test;
+
+public class CustomerTest {
+	
+	@Test
+	public void createCustomerTest(){
+		System.out.println("execute createCustomerTest");
+	}
+
+	@Test
+	public void modifyCustomerTest(){
+		System.out.println("execute modifyCustomerTest");
+	}
+
+	@Test
+	public void deleteCustomerTest(){
+		System.out.println("execute deleteCustomerTest");
+	}
+	@Test
+	public void searchCustomerTest(){
+		System.out.println("execute searchCustomerTest");
+	}
+
+}
